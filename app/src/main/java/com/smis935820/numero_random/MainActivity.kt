a8ecf9873a0e320892e25ebf4ab99d2ada9cca6a
AmLopez <soft2320@gmail.com>
2021-05-17 12:14:13 -0600

    Base del Proyecto

---
+++ app/src/main/java/com/smis935820/numero_random/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.smis935820.numero_random
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
